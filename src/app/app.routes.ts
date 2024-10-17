8d738eee799aa79015e73fd010020c13335cd37c
lautaroeberle <louloufutbol@gmail.com>
2024-10-17 14:53:44 -0300

    agregué rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
-import { InicioComponent } from './inicio/inicio.component';
-
+import { InicioComponent } from './components/Secciones/inicio/inicio.component';
+import { SeccionNoticiasComponent } from "./components/Secciones/seccion-noticias/seccion-noticias.component";
+import { SeccionCalendarioComponent } from './components/Secciones/seccion-calendario/seccion-calendario.component';
+import { SeccionTorneosComponent } from './components/Secciones/seccion-torneos/seccion-torneos.component';
+import { SeccionJugadoresComponent } from './components/Secciones/seccion-jugadores/seccion-jugadores.component';
 export const routes: Routes = [
     {
         path: "",
@@ -10,5 +13,21 @@ export const routes: Routes = [
     {
         path: "inicio",
         component: InicioComponent,
-    }
+    },
+    {
+        path: "noticias",
+        component: SeccionNoticiasComponent,
+    },
+    {
+        path: "torneos",
+        component: SeccionTorneosComponent,
+    },
+    {
+        path: "calendario",
+        component: SeccionCalendarioComponent,
+    },
+    {
+        path: "jugadores",
+        component: SeccionJugadoresComponent,
+    },
 ];
---
+++ src/app/components/Secciones/seccion-jugadores/seccion-jugadores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seccion-jugadores',
+  standalone: true,
+  imports: [],
+  templateUrl: './seccion-jugadores.component.html',
+  styleUrl: './seccion-jugadores.component.css'
+})
+export class SeccionJugadoresComponent {
+
+}
