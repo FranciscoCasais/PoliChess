ca62650ed972800df904e4971079df128d21eb77
lautaroeberle <louloufutbol@gmail.com>
2024-11-14 14:14:08 -0300

    si seba nos vamos antes

--- FrontPoliChess/src/app/components/Secciones/inicio/inicio.component.ts
+++ FrontPoliChess/src/app/components/Secciones/inicio/inicio.component.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Noticia } from '../../../../models/noticia.model';
+import { NoticiaService } from '../../../../services/noticia.service';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 
 export class InicioComponent {
-  public scrollear() {
+  protected i: number = 0;
+  private noticiaService: NoticiaService = inject(NoticiaService);
+  protected noticias: Noticia[] = this.noticiaService.obtenerNoticias();
+
+  public verMas(): void {
     document.getElementById("noticias")?.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
+      behavior: "smooth",
+      block: "start"
     });
+  }
+
+  public cambiarNoticia(siguiente: boolean) {
+    if(!siguiente && this.i > 0) {
+      this.i--;
+    } else if(!siguiente) {
+      this.i = this.noticias.length - 1;
+    } else if(siguiente && this.i < this.noticias.length - 1) {
+      this.i++;
+    } else {
+      this.i = 0;
+    }
   }
 }
